f939cab2140ec13f2fb3b562c2eec24374f55c94
ludovic.feltz <ludovic.feltz@gmail.com>
2022-04-06 15:54:24 +0200

    Added test for ADS1115 on I2C

--- libMPSSEWrapper/Test/Program.cs
+++ libMPSSEWrapper/Test/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using libMPSSEWrapper;
+using libMPSSEWrapper.I2c;
 using libMPSSEWrapper.Types;
 
 namespace Test
@@ -12,8 +13,34 @@ namespace Test
 
         static void Main(string[] args)
         {
+            /*
+             * I2C Example
+             */
+            var adcI2cConfig = new FtI2cChannelConfig
+            {
+                ClockRate = FtI2cClockRate.I2C_CLOCK_FAST_MODE,
+                LatencyTimer = LatencyTimer
+            };
 
-            var adcSpiConfig = new FtChannelConfig
+            var i2cConfig = new FtI2cConfiguration(Ads1115.ADS_ADDR_SDA, 0);
+
+            var adcConfig = new Ads1115Configuration()
+            {
+                Gain = Ads1115.Gain.One
+            };
+            
+            var adc = new Ads1115(adcConfig, adcI2cConfig, i2cConfig);
+            do
+            {
+                Console.WriteLine("Channel 0: " + adc.GetConvertedSampleFrom(Ads1115.Channel.Channel0));
+                Console.WriteLine("Channel 1: " + adc.GetConvertedSampleFrom(Ads1115.Channel.Channel1));
+                Console.WriteLine("Channel 2: " + adc.GetConvertedSampleFrom(Ads1115.Channel.Channel2));
+            } while (true);
+
+            /*
+             *  SPI Example 
+             *
+            var adcSpiConfig = new FtSpiChannelConfig
                              {
                                  ClockRate = ConnectionSpeed,
                                  LatencyTimer = LatencyTimer,
@@ -21,7 +48,7 @@ namespace Test
                              };
 
           
-
+            
             var adcConfig = new Maxim186Configuration
                                 {
                                     Channel = Maxim186.Channel.Channel0,
